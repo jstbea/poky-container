3b0840cbcf3c50c6320d2b7f3d2a9624f7c3883e
TLA-BEA <tla@bea.be>
2021-05-05 21:03:57 +0200

    sigma meta-layers cloning

--- custom-init.sh
+++ custom-init.sh
@@ -8,7 +8,19 @@ else
     /usr/bin/git clone git://git.yoctoproject.org/poky .
 
     ##### CUSTOM SETUP GOES HERE #####
+    echo "Checking out morty-16.0.4"
     /usr/bin/git checkout tags/morty-16.0.4 -b bea-morty
+    echo "Cloning BEA's meta-layers"
+    /usr/bin/git clone git@github.com:bea-sensors/meta-bea.git
+    /usr/bin/git clone git@github.com:bea-sensors/meta-bea-bsp.git
+    /usr/bin/git clone git@github.com:bea-sensors/meta-bea-peco.git
+    /usr/bin/git clone git@github.com:bea-sensors/meta-freescale-bea.git
+    /usr/bin/git clone git@github.com:bea-sensors/meta-mender-bea.git
+    /usr/bin/git clone git@github.com:bea-sensors/meta-oe-bea.git
+    /usr/bin/git clone git@github.com:bea-sensors/meta-qt5-bea.git
+    /usr/bin/git clone git@github.com:bea-sensors/meta-security-bea.git
+    /usr/bin/git clone git@github.com:bea-sensors/oe-meta-go-bea.git
+    /usr/bin/git clone git@github.com:bea-sensors/openembedded-core-bea.git
     ##################################
 fi
 
