82af58e1b9f4261e63545555fde92e69911c860a
TLA-BEA <tla@bea.be>
2021-05-06 17:31:18 +0200

    devops migration

--- custom-init.sh
+++ custom-init.sh
@@ -5,24 +5,22 @@ if [ "$(ls -A $PWD)" ]; then
      /bin/echo "$PWD is not Empty, Ready!"
 else
     /bin/echo "$PWD is Empty, cloning yocto"
-    /usr/bin/git clone git://git.yoctoproject.org/poky .
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-yocto-bea.git . -b sigma
 
     ############### CUSTOM SETUP GOES HERE ###############
-    /bin/echo "Checking out morty-16.0.4"
-    /usr/bin/git checkout tags/morty-16.0.4 -b bea-morty
 
     /bin/echo "Cloning BEA's meta-layers"
-    /usr/bin/git clone git@github.com:bea-sensors/meta-bea.git
-    /usr/bin/git clone git@github.com:bea-sensors/meta-bea-bsp.git
-    /usr/bin/git clone git@github.com:bea-sensors/meta-bea-peco.git
-    /usr/bin/git clone git@github.com:bea-sensors/meta-freescale-bea.git
-    /usr/bin/git clone git@github.com:bea-sensors/meta-mender-bea.git -b morty
-    /usr/bin/git clone git@github.com:bea-sensors/meta-oe-bea.git
-    /usr/bin/git clone git@github.com:bea-sensors/meta-qt5-bea.git
-    /usr/bin/git clone git@github.com:bea-sensors/meta-security-bea.git -b morty
-    /usr/bin/git clone git@github.com:bea-sensors/oe-meta-go-bea.git
-    /usr/bin/git clone git@github.com:bea-sensors/openembedded-core-bea.git -b morty
-    ##################################
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-bea.git
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-bea-bsp.git
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-bea-peco.git
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-freescale-bea.git
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-mender-bea.git -b morty
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-oe-bea.git
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-qt5-bea.git
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-security-bea.git -b morty
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/oe-meta-go-bea.git
+    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/openembedded-core-bea.git -b morty
+
     #####################################################
 fi
 
