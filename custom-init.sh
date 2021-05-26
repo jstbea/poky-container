14ba3ca6da15f3f6b36495c19aaf1d6fce38bdb7
TLA-BEA <tla@bea.be>
2021-05-26 14:57:46 +0200

    copy custom bblayers.conf/local.conf to builddir
    w

--- custom-init.sh
+++ custom-init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+CPSAMPLES="0"
 # If the workdir is empty, clone poky and initialize layers
 if [ "$(ls -A $PWD)" ]; then
      /bin/echo "$PWD is not Empty, Ready!"
 else
     /bin/echo "$PWD is Empty, cloning yocto"
-    /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-yocto-bea.git . -b sigma
+    # /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/meta-yocto-bea.git . -b morty
+    git clone git://git.yoctoproject.org/poky . -b morty
 
     ############### CUSTOM SETUP GOES HERE ###############
 
@@ -21,8 +23,14 @@ else
     /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/oe-meta-go-bea.git
     /usr/bin/git clone ssh://git@ssh.dev.azure.com/v3/BEA-RD-Firmware/E363_Sigma/openembedded-core-bea.git -b morty
 
+    CPSAMPLES="1"
     #####################################################
 fi
 
 source ./oe-init-build-env
+
+if [ $CPSAMPLES = "1" ]; then
+    cp /workdir/meta-bea/conf/local.conf.sample /workdir/build/conf/local.conf
+    cp /workdir/meta-bea/conf/bblayers.conf.sample /workdir/build/conf/bblayers.conf
+fi
 exec /bin/bash
