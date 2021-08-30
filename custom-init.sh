550ef294b67905334246fc5e6c959cbfc61b822f
STORDEUR Jordan <jst@bea.be>
2021-08-30 16:47:31 +0200

    Add samples files

--- custom-init.sh
+++ custom-init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CPSAMPLES="0"
+
 # If the workdir is empty, clone poky and initialize layers
 if [ "$(ls -A $PWD)" ]; then
      echo "$PWD is not Empty, Ready!"
@@ -15,11 +17,18 @@ else
     chmod a+x /workdir/bin/repo
 
     # F115 Manifest
-    repo init -u git@ssh.dev.azure.com:v3/BEA-RD-Firmware/F115-Camera_ToF/f115_manifest -b master -m tdxref/default.xml
+    repo init -u git@ssh.dev.azure.com:v3/BEA-RD-Firmware/F115-Camera_ToF/f115_manifest -b dunfell-5.x.y -m bea/default.xml
     repo sync
 
+    CPSAMPLES="1"
     ##################################
 fi
 
 source export build
+
+if [ $CPSAMPLES = "1" ]; then
+    cp "/workdir/layers/meta-bea-bsp/conf/local.conf.sample" "/workdir/build/conf/local.conf"
+    cp "/workdir/layers/meta-bea-bsp/conf/bblayers.conf.sample" "/workdir/build/conf/bblayers.conf"
+fi
+
 exec /bin/bash
